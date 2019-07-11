85e4fe53c4f3a4ab26fc434d69be0ece0b8bbdb6
sepuckett86 <sepuckett86@gmail.com>
2019-07-11 16:32:07 -0700

    pass id test

--- test/app.test.js
+++ test/app.test.js
@@ -23,7 +23,27 @@ describe('app', () => {
     return request(app)
       .get('/api/v1/instruments')
       .then(res => {
-        expect(res.body).toEqual([]);
+        expect(res.body).toEqual([
+          {
+            name: 'clarinet',
+            type: 'woodwind',
+            price: 1000
+          }
+        ]);
+      });
+  });
+
+  it('/api/v1/instrument/:id returns an instrument', () => {
+    return request(app)
+      .get('/api/v1/instruments/0')
+      .then(res => {
+        expect(res.body).toEqual(
+          {
+            name: 'clarinet',
+            type: 'woodwind',
+            price: 1000
+          }
+        );
       });
   });
 });
